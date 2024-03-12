ab9a2e78f95c99a74871a67e591f5a763461ecac
seba-wetzel <seba.wetzel@gmail.com>
2024-03-12 03:20:46 -0300

    Fix: validacion de typescript

--- src/app/denuncia/page.tsx
+++ src/app/denuncia/page.tsx
@@ -1,31 +1,47 @@
-import { Button } from "@/components/ui/button";
-import { FC } from "react";
+// import { Button } from "@/components/ui/button";
+// import
+// import { FC } from "react";
 
-const Denuncia: FC = () => {
-  return (
-    <div>
-      <h1>Denuncia</h1>
-      <form></form>
-      <div>
-        <label htmlFor="denuncia">Denuncia</label>
-        <textarea id="denuncia" name="denuncia" />
-      </div>
-      <div>
-        <label htmlFor="nome">Nome</label>
-        <input id="nome" name="nome" type="text" />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" />
-      </div>
-      <div>
-        <label htmlFor="telefone">Telefone</label>
-        <input id="telefone" name="telefone" type="tel" />
-      </div>
-      <div>
-        <Button type="submit">Enviar</Button>
-      </div>
-    </div>
-  );
-};
-export default Denuncia;
+// const Denuncia: FC = () => {
+//   return (
+//     <div>
+//       <h1>Denuncia</h1>
+//       <Form>
+//   <FormField
+//     control={...}
+//     name="..."
+//     render={() => (
+//       <FormItem>
+//         <FormLabel />
+//         <FormControl>
+//           { /* Your form field */}
+//         </FormControl>
+//         <FormDescription />
+//         <FormMessage />
+//       </FormItem>
+//     )}
+//   />
+// </Form>
+//       <div>
+//         <label htmlFor="denuncia">Denuncia</label>
+//         <textarea id="denuncia" name="denuncia" />
+//       </div>
+//       <div>
+//         <label htmlFor="nome">Nome</label>
+//         <input id="nome" name="nome" type="text" />
+//       </div>
+//       <div>
+//         <label htmlFor="email">Email</label>
+//         <input id="email" name="email" type="email" />
+//       </div>
+//       <div>
+//         <label htmlFor="telefone">Telefone</label>
+//         <input id="telefone" name="telefone" type="tel" />
+//       </div>
+//       <div>
+//         <Button type="submit">Enviar</Button>
+//       </div>
+//     </div>
+//   );
+// };
+// export default Denuncia;
