4762ab0e07f0381447614c7622a5062a997a2848
seba-wetzel <seba.wetzel@gmail.com>
2024-03-13 21:07:56 -0300

    Fix: dialog mantiene el estado en la url para comisarias y para casos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Dialog from "@/components/Dialog";
-import type { PuntoOnClick } from "@/components/Mapa";
 import { Mapa } from "@/components/Mapa";
 import { GeoJsonProperties, type FeatureCollection, type Point } from "geojson";
 import Image from "next/image";
@@ -12,55 +11,58 @@ import geoSeccionales from "@/data/geo/seccionales-la_plata.json";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [open, setOpen] = useState(false);
-
   const [showCasos, setShowCasos] = useState(false);
   const [selectedCaso, setSelectedCaso] = useState<GeoJsonProperties | null>(
     null,
   );
+  const [selectedComisaria, setSelectedComisaria] =
+    useState<GeoJsonProperties | null>(null);
 
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const [data, setData] = useState({
-    title: "",
-    content: "",
-    tel: "",
-    representante: "",
-  });
-
-  function handleSearch(term: string) {
+  function handleSearch(
+    term: string,
+    type: "comisaria" | "caso" = "comisaria",
+  ) {
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set("comisaria", term);
+      params.set(type, term);
     } else {
+      params.delete("caso");
       params.delete("comisaria");
     }
     replace(`${pathname}?${params.toString()}`);
   }
 
-  const puntoHandlerClick: PuntoOnClick = (data, id) => {
-    setOpen(true);
-    const { icon, ...properties } = data.properties;
-    handleSearch(JSON.stringify(properties));
+  function handleCasoClick(properties: GeoJsonProperties) {
+    handleSearch(JSON.stringify(properties), "caso");
+  }
+
+  const comisariaHandlerClick = (properties: GeoJsonProperties) => {
+    //Hay que sacar el icono del objeto properties
+    //para np generar una redundancia en json.stringify
+    const { icon, ...data } = properties;
+    handleSearch(JSON.stringify(data), "comisaria");
   };
 
   useEffect(() => {
-    if (searchParams.get("comisaria")) {
-      const paramsFound = searchParams.get("comisaria");
-      if (!paramsFound) return;
-      const data = JSON.parse(paramsFound);
-      setData({
-        title: data.Nombre || "Comisaria",
-        content: data["Dirección"] || "Sin Direccion",
-        tel: data["Teléfono"] || "Sin Telefono",
-        representante: data["Representa"] || "Sin Representante",
-      });
-      setOpen(true);
+    const comisaria = searchParams.get("comisaria");
+    const caso = searchParams.get("caso");
+    if (comisaria) {
+      const data = JSON.parse(comisaria);
+      console.log(data);
+      setSelectedComisaria(data);
       return;
     }
-    setOpen(false);
+    if (caso) {
+      const data = JSON.parse(caso);
+      setSelectedCaso(data);
+      return;
+    }
+    setSelectedComisaria(null);
+    setSelectedCaso(null);
   }, [searchParams]);
 
   return (
@@ -92,30 +94,42 @@ export default function Home() {
               width={24}
             />
           }
-          onClick={puntoHandlerClick}
+          onClick={({ properties }) => comisariaHandlerClick(properties)}
         />
         {showCasos && (
           <Mapa.Puntos
             data={casos as FeatureCollection<Point>}
             id="my-data-2"
-            onClick={(c) => setSelectedCaso(c.properties)}
+            onClick={({ properties }) => handleCasoClick(properties)}
           />
         )}
       </Mapa>
 
       <Dialog
-        open={Boolean(selectedCaso)}
-        onClose={() => setSelectedCaso(null)}
+        open={Boolean(selectedComisaria)}
+        onClose={() => handleSearch("")}
       >
+        {selectedComisaria && (
+          <div>
+            <h1>Nombre: {selectedComisaria["Nombre"]}</h1>
+            <p>Direccion: {selectedComisaria["Dirección"]}</p>
+            <p>Telefono: {selectedComisaria["Teléfono"]}</p>
+            <p>Representante: {selectedComisaria["Representa"]}</p>
+          </div>
+        )}
+      </Dialog>
+
+      <Dialog open={Boolean(selectedCaso)} onClose={() => handleSearch("")}>
         {selectedCaso && (
           <div className="max-w-md">
-            <h1>Caso nro: {selectedCaso["NumCaso"]}</h1>
-            <div className="flex flex-row flex-wrap">
+            <div className="my-4 flex flex-row flex-wrap gap-4">
+              <p>Caso nro: {selectedCaso["NumCaso"]}</p>
               <p>Fecha: {selectedCaso["Fecha de Deceso"]}</p>
               <p>Barrio: {selectedCaso["Ciudad"]}</p>
               <p>Nombre: {selectedCaso["Nombre"]}</p>
             </div>
             <h6>Situación Procesal: {selectedCaso["Situación Procesal"]}</h6>
+            <h6 className="mt-4 text-center font-bold">Circunstancias</h6>
             <p>{selectedCaso["Circunstancias"]}</p>
           </div>
         )}
